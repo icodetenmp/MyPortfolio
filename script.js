48cad5d2a75a29d5c07ff455324a145ee135bb56
temi <icodetenmp@gmail.com>
2026-03-12 01:41:47 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+const text = document.querySelector(".word");
+const hamburger = document.querySelector(".fa-solid");
+const navbar = document.getElementById("navBar")
+
+const words = "Junior full stack developer || virtual-assistant I build web applications and help businesses run smoothly.\nWhether its coding a feature or managin task, bring dedication and problem solving to every thing i do.\n Lets waalk through knowing me.";
+
+let index = 0;
+
+let switchy = 0;
+
+function typeWriter( ){
+    if (index < words.length){
+        text.innerHTML += words.charAt(index);
+        index++;
+        setTimeout(typeWriter, 50)
+    }
+}
+typeWriter();
+
+
+
+hamburger.addEventListener("click", ()=>{
+    if(switchy === 0){
+        navbar.style.visibility = "visible";
+        switchy++;
+    }else{
+        navbar.style.visibility = "hidden";
+        switchy = 0;
+    }
+
+    
+
+
+
+})
